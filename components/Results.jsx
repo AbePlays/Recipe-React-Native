7d768fea0debf1030751a9e4953790c2d08a9a52
Abhishek Rawat <47311875+AbePlays@users.noreply.github.com>
2020-09-16 12:39:03 +0530

    Update Results.jsx

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { ScrollView } from "react-native";
 import { View, ActivityIndicator } from "react-native";
+import Header from "./Header";
 import RecipeCard from "./RecipeCard";
 
 function Results({ route, navigation }) {
@@ -11,6 +13,8 @@ function Results({ route, navigation }) {
   const apiKey = "12a8a1f925c236c992926c9ef4955e39";
   const url = `https://api.edamam.com/search?q=${food}&app_id=${appID}&app_key=${apiKey}`;
 
+  const color = ["#fccbcb", "#c8d5b9"];
+
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
@@ -20,22 +24,31 @@ function Results({ route, navigation }) {
   }, []);
 
   return (
-    <View style={{ flex: 1 }}>
-      {loading ? (
-        <ActivityIndicator
-          size="large"
-          color="green"
-          style={{ flex: 1, justifyContent: "center", alignContent: "center" }}
-        />
-      ) : (
-        data.map((stuff) => (
-          <RecipeCard
-            title={stuff.recipe.label}
-            url={stuff.recipe.image}
-            lots={stuff.recipe.ingredientLines}
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      {loading ? <></> : <Header navigation={navigation} />}
+      <ScrollView style={{ marginTop: 20, flex: 1 }}>
+        {loading ? (
+          <ActivityIndicator
+            size="large"
+            color="grey"
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignContent: "center",
+            }}
           />
-        ))
-      )}
+        ) : (
+          data.map((stuff, index) => (
+            <RecipeCard
+              key={index}
+              bgColor={color[index % 2]}
+              title={stuff.recipe.label}
+              url={stuff.recipe.image}
+              lots={stuff.recipe.ingredientLines}
+            />
+          ))
+        )}
+      </ScrollView>
     </View>
   );
 }
